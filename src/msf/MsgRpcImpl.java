ae71be733260d82c7a094a2710b7a13823b254de
LBCRP-OWNER <robertalredro@gmail.com>
2025-12-31 02:37:39 -0500

    Update MsgRpcImpl.java
    Clean up, Squashed some bugs.

--- src/msf/MsgRpcImpl.java
+++ src/msf/MsgRpcImpl.java
@@ -1,5 +1,4 @@
 package msf;
-
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -7,149 +6,137 @@ import javax.net.ssl.*;
 import org.msgpack.*;
 import org.msgpack.type.*;
 import org.msgpack.packer.*;
-
-/**
- * Taken from BSD licensed msfgui by scriptjunkie.
- *
- * Implements an RPC backend using the MessagePack interface
- * @author scriptjunkie
- */
 public class MsgRpcImpl extends RpcConnectionImpl {
-	private URL u;
-	private URLConnection huc; // new for each call
-	protected int timeout = 0; /* scriptjunkie likes timeouts, I don't. :) */
-	protected MessagePack packer = new MessagePack();
-
-	/**
-	 * Creates a new URL to use as the basis of a connection.
-	 */
-	public MsgRpcImpl(String username, String password, String host, int port, boolean ssl, boolean debugf) throws MalformedURLException {
-		if (ssl) { // Install the all-trusting trust manager & HostnameVerifier
+	private URL apiUrl;
+	private static final MessagePack messagePack = new MessagePack();
+	private int connectTimeout = 15000; 
+	private int readTimeout = 90000;    
+	public static class MsfRpcException extends RuntimeException {
+		public final Map<Value, Value> errorData;
+		public MsfRpcException(String message, Map<Value, Value> errorData) {
+			super(message);
+			this.errorData = errorData;
+		}
+		public String getErrorClass() {
+			return errorData.get(new RawValue("error_class")).toString();
+		}
+		public String getErrorBacktrace() {
+			return errorData.get(new RawValue("error_backtrace")).toString();
+		}
+	}
+	public MsgRpcImpl(String username, String password, String host, int port, boolean ssl, boolean debugf) throws Exception {
+		if (ssl) {
 			try {
-				SSLContext sc = SSLContext.getInstance("SSL");
+				SSLContext sc = SSLContext.getInstance("TLS"); 
 				sc.init(null, new TrustManager[] {
 					new X509TrustManager() {
-						public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-							return null;
-						}
-						public void checkClientTrusted(
-							java.security.cert.X509Certificate[] certs, String authValue) {
-						}
-						public void checkServerTrusted(
-							java.security.cert.X509Certificate[] certs, String authValue) {
-						}
+						public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
+						public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {}
+						public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {}
 					}
 				}, new java.security.SecureRandom());
-
 				HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-				HttpsURLConnection.setDefaultHostnameVerifier( new HostnameVerifier() {
-					public boolean verify(String string,SSLSession ssls) {
-						return true;
-					}
-				});
+				HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true); 
+			} catch (Exception e) {
+				throw new RuntimeException("Could not install the all-trusting SSL manager.", e);
 			}
-			catch (Exception e) {
-			}
-			u = new URL("https", host, port, "/api/1.0/");
+			apiUrl = new URL("https", host, port, "/api/1.0/");
+		} else {
+			apiUrl = new URL("http", host, port, "/api/1.0/");
 		}
-		else {
-			u = new URL("http", host, port, "/api/1.0/");
+		Map results = exec("auth.login", new Object[]{username, password});
+		if (results == null || !results.containsKey("token")) {
+			throw new SecurityException("Authentication failed. Check your credentials.");
 		}
-
-		/* login to msf server */
-		Object[] params = new Object[]{ username, password };
-		Map results = exec("auth.login",params);
-
-		/* save the temp token (lasts for 5 minutes of inactivity) */
-		rpcToken = results.get("token").toString();
-
-		/* generate a non-expiring token and use that */
-		params = new Object[]{ rpcToken };
-		results = exec("auth.token_generate", params);
-		rpcToken = results.get("token").toString();
+		String tempToken = results.get("token").toString();
+		results = exec("auth.token_generate", new Object[]{tempToken});
+		if (results == null || !results.containsKey("token")) {
+			throw new SecurityException("Failed to generate a permanent token. The server is probably having a bad day.");
+		}
+		this.rpcToken = results.get("token").toString();
 	}
-
-	/**
-	 * Decodes a response recursively from MessagePackObject to a normal Java object
-	 * @param src MessagePack response
-	 * @return decoded object
-	 */
-	private static Object unMsg(Object src){
-		Object out = src;
-		if (src instanceof ArrayValue) {
-			List l = (ArrayValue)src;
-			List outList = new ArrayList(l.size());
-			out = outList;
-			for(Object o : l)
+	private static Object unMsg(Value src) {
+		if (src.isArrayValue()) {
+			List<Value> l = src.asArrayValue().getElementList();
+			List<Object> outList = new ArrayList<>(l.size());
+			for(Value o : l)
 				outList.add(unMsg(o));
+			return outList;
 		}
-		else if (src instanceof BooleanValue) {
-			out = ((BooleanValue)src).getBoolean();
+		if (src.isBooleanValue()) {
+			return src.asBooleanValue().getBoolean();
 		}
-		else if (src instanceof FloatValue) {
-			out = ((FloatValue)src).getFloat();
+		if (src.isFloatValue()) {
+			return src.asFloatValue().getDouble(); 
 		}
-		else if (src instanceof IntegerValue) {
+		if (src.isIntegerValue()) {
+			return src.asIntegerValue().getBigInteger();
+		}
+		if (src.isMapValue()) {
+			Map<Value, Value> srcMap = src.asMapValue().map();
+			Value errorClass = new RawValue("error_class");
+			if (srcMap.containsKey(errorClass)) {
+				String errorMessage = srcMap.get(new RawValue("error_message")).toString();
+				throw new MsfRpcException(errorMessage, srcMap);
+			}
+			Map<String, Object> out = new HashMap<>();
+			for (Map.Entry<Value, Value> entry : srcMap.entrySet()) {
+				String key = unMsg(entry.getKey()).toString(); 
+				Value val = entry.getValue();
+				if(srcMap.size() == 1 && val.isRawValue() && (key.equals("payload") || key.equals("encoded"))) {
+					out.put(key, val.asRawValue().getByteArray());
+				} else {
+					out.put(key, unMsg(val));
+				}
+			}
+			return out;
+		}
+		if (src.isNilValue()) {
+			return null;
+		}
+		if (src.isRawValue()) {
 			try {
-				out = ((IntegerValue)src).getInt();
-			}
-			catch (Exception ex) {
-				/* this is a bandaid until I have a chance to further examine what's happening */
-				out = ((IntegerValue)src).getLong();
+				return src.asRawValue().getString();
+			} catch (UnsupportedEncodingException e) {
+				return src.asRawValue().getByteArray();
 			}
 		}
-		else if (src instanceof MapValue) {
-			Set ents = ((MapValue)src).entrySet();
-			out = new HashMap();
-			for (Object ento : ents) {
-				Map.Entry ent = (Map.Entry)ento;
-				Object key = unMsg(ent.getKey());
-				Object val = ent.getValue();
-				// Hack - keep bytes of generated or encoded payload
-				if(ents.size() == 1 && val instanceof RawValue && (key.equals("payload") || key.equals("encoded")))
-					val = ((RawValue)val).getByteArray();
-				else
-					val = unMsg(val);
-				((Map)out).put(key + "", val);
-			}
-
-			if (((Map)out).containsKey("error") && ((Map)out).containsKey("error_class")) {
-				armitage.ArmitageMain.print_error("Metasploit Framework Exception: " + ((Map)out).get("error_message").toString() + "\n" + ((Map)out).get("error_backtrace"));
-				throw new RuntimeException(((Map)out).get("error_message").toString());
-			}
-		}
-		else if (src instanceof NilValue) {
-			out = null;
-		}
-		else if (src instanceof RawValue) {
-			out = ((RawValue)src).getString();
-		}
-		return out;
+		return src; 
 	}
-
-	/** Creates an XMLRPC call from the given method name and parameters and sends it */
 	protected void writeCall(String methodName, Object[] args) throws Exception {
-		huc = u.openConnection();
+		HttpURLConnection huc = (HttpURLConnection) apiUrl.openConnection();
 		huc.setDoOutput(true);
 		huc.setDoInput(true);
 		huc.setUseCaches(false);
+		huc.setRequestMethod("POST");
 		huc.setRequestProperty("Content-Type", "binary/message-pack");
-		huc.setReadTimeout(0);
-		OutputStream os = huc.getOutputStream();
-
-		LinkedList temp = new LinkedList();
-		temp.add(methodName);
-		for (Object o : args)
-			temp.add(o);
-
-		packer.write(os, temp);
-		os.close();
+		huc.setConnectTimeout(this.connectTimeout);
+		huc.setReadTimeout(this.readTimeout);
+		List<Object> call = new LinkedList<>();
+		call.add(methodName);
+		Collections.addAll(call, args);
+		try (OutputStream os = huc.getOutputStream()) {
+			messagePack.write(os, call);
+		}
+		this.huc = huc; 
 	}
-
-	/** Receives an RPC response and converts to an object */
 	protected Object readResp() throws Exception {
-		InputStream is = huc.getInputStream();
-		Value mpo = packer.read(is);
-		return unMsg(mpo);
+		if (this.huc == null) {
+			throw new IOException("readResp called before writeCall.");
+		}
+		try (InputStream is = this.huc.getInputStream()) {
+			Value mpo = messagePack.read(is);
+			return unMsg(mpo);
+		} catch (IOException e) {
+			try (InputStream es = ((HttpURLConnection) this.huc).getErrorStream()) {
+				if (es != null) {
+					Value mpo = messagePack.read(es);
+					return unMsg(mpo); 
+				}
+			}
+			throw e; 
+		} finally {
+			this.huc = null; 
+		}
 	}
 }
